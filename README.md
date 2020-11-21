7d7be5e34e84e3e07b1e1d1adcab2f9b6ffe3304
Faidon Kourounakis <faidon.kourounakis@gmail.com>
2020-11-21 14:50:44 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,195 @@
+# Amuse.js 
+Simple \<audio> wrapper library to play sounds/music, using async/await on the frontend!
+## Install
+```bash
+npm install amuse --save
+```
+```javascript
+import Amuse from 'amuse';
+```
+## Quickstart
+Initialise amuse player without any songs (aka muses) and add your first muse:
+```javascript
+let src = 'https://res.cloudinary.com/faidondev/video/upload/v1576323386/Maelstrom%20high%20quality/een%20flamboyant%20gevoelsfestijn/15_Het_groot_Aardbeienlied_uyu0xu.mp3',
+    id = 'song1',
+    meta = { title: 'Aardbeilied', artist: 'Maelstrom', album: 'Flamboyant Gevoelsfestijn'},
+    load = true;
+
+let amuse = new Amuse();
+amuse.addMuse(src, id, meta, load);
+
+// shorthand:
+let amuse = new Amuse(srd, id, meta, load);
+
+await amuse.play();
+```
+
+`id`: (required) Unique identifier of muse (song or sound) which you'll need later on.
+`src`: URL to mp3 file.
+`meta`: (optional) object with meta data of mp3 file (or really whatever data you wanna store). 
+`load`: (optional, defaults to true) if true loads audio file upon muse addition, if false loads audio file later (automatically when played or manually ...read further).
+
+## Methods
+#### Muses
+
+```javascript
+await amuse.addMuse(src, id, meta, load);
+```
+Adds new muse at the end of the playlist.
+
+```javascript
+await amuse.removeMuseById(id);
+```
+Removes muse by `id`, stops it if its the current one and selects the next muse or the previous one (if the latter doesn't exist).
+
+```javascript
+amuse.geMuseById(id); // of the few sync methods
+```
+Returns muse object that contains `src`, `id`, `meta` and the corresponding `audio` \<audio> element. 
+
+```javascript
+await amuse.empty(); 
+```
+Removes all muses from playlist.
+```javascript
+await amuse.reset(); 
+```
+Empties playlist, sets amuse properties to defaults, removes event listeners and UI elements. 
+*This method is work in progress. Do not use it in production builds.*
+
+#### Controls
+
+```javascript
+await amuse.load(id);
+```
+
+If `id` is defined, loads muse with `id`. Otherwise loads currently selected muse.
+```javascript
+await amuse.play();
+```
+
+```javascript
+await amuse.pause();
+```
+
+```javascript
+await amuse.stop();
+```
+Pauses and resets song to 0:00.
+
+```javascript
+await amuse.next();
+```
+Return false if there is no muse after current one (otherwise true and plays it).
+
+```javascript
+await amuse.previous(); 
+```
+Same behaviour with `.next()`.
+
+```javascript
+await amuse.skipToIndex(index); 
+
+```
+Skips to muse with `index` (starts from 0).
+
+```javascript
+await amuse.skipToId(id); 
+```
+Skips to muse with `id`.
+
+#### Events
+
+```javascript
+amuse.addEventListener(event, callback); 
+// same as 
+amuse.on(event, callback);
+// or to only fire once:
+amuse.once(event, callback);
+```
+Callback function receives amuse instance as argument.
+`event` is one of the following:
+- `'play'`: fires when a muse has started playing,
+- `'pause'`: fires when a muse is paused,
+- `'stop'`: fires at stop,
+- `'seek'`: fires when muse is being seeked,
+- `'next'`: fires when the next muse gets played,
+- `'previous'`: fires when the previous muse gets played,
+- `'skip'`: fires when the playing muse changes,
+- `'playing'`: fires constantly while a muse is playing,
+- `'load'`: fires when a muse is loaded,
+- `'error'`: fires when a muses \<audio> element fires an error.
+
+```javascript
+amuse.removeEventListener(event, callback); 
+// same as
+amuse.notOn(event, callback);
+```
+
+#### UI Elements
+
+```javascript
+amuse.addSeekBar(outer, inner, initialPercentage);
+```
+This method can be used to add one (or multiple) seek bar which the user can drag (to seek) and which gets updated every second. 
+- `outer`: (DOM element) element with the maximum (fixed) width of the bar
+- `inner`: (DOM element) seek bar that is contained in outer and will be transformed. (`inner.style.width = '--%'` is used)
+- `initialPercentage`: the initial width (in %) of `inner` when nothing is playing yet.
+
+```javascript
+amuse.addElement(type, el);
+```
+This method allows you to automaticaly update timestamps in the `innerText` of elements.
+`type` is one of the following:
+- `'currentTime'`: `innerText` will be the current time of the selected muse (e.g. 1:01 if the selected muse is at 1m1s)
+- `'durationTime'`: innerText will be the full length of the selected muse (e.g. 3:45 if the total length is 3m45s)
+- `'completeTime'`: `currentTime` and `durationTime` combined (for the aformentioned values this element's innerText would be 1:01/3:45)
+`el`: the DOM element.
+
+## Properties
+
+#### Writable
+
+```javascript
+amuse.autoPlay = true;
+```
+If true, after the end of a muse, the next one is automatically played. Defaults to `false`.
+
+```javascript
+amuse.rememberTime = false;
+```
+If true, when skipping to another muse and returning to the first one, muse will continue playing from where it had left of. Defaults to `true`.
+
+```javascript
+let t = amuse.currentSeconds
+amuse.currentSeconds = 89;
+```
+Get and set current time of selected muse in seconds.
+
+```javascript
+let p = amuse.currentPercentage
+amuse.currentSeconds = .75;
+```
+Get and set current time of selected muse in percentage (<=1 and >=0).
+
+#### Read only
+
+```javascript
+amuse.currentMuse
+```
+Current muse object (contains src, id, meta, audio element)
+
+```javascript
+amuse.loaded
+```
+True if selected muse has loaded
+
+```javascript
+amuse.playing
+amuse.paused // same as !amuse.playing
+```
+```javascript
+amuse.duration
+```
+Duration (in seconds) of selected muse.
+
---
+++ amuse.js
@@ -0,0 +1,495 @@
+function onmousehold(el, fn, t = 100) {
+  let eventCopy,
+    holdX,
+    holdY,
+    pressed = false;
+
+  function updateMouse(e) {
+    eventCopy = e;
+    holdX = e.clientX;
+    holdY = e.clientY;
+  }
+  function updateTouch(e) {
+    eventCopy = e;
+    holdX = e.touches[0].clientX;
+    holdY = e.touches[0].clientY;
+  }
+  function insertListeners() {
+    window.addEventListener("touchmove", updateTouch);
+    window.addEventListener("mousemove", updateMouse);
+    window.addEventListener("touchend", stopUpdate);
+    window.addEventListener("mouseup", stopUpdate);
+  }
+  function ejectListeners() {
+    window.removeEventListener("touchmove", updateTouch);
+    window.removeEventListener("mousemove", updateMouse);
+    window.removeEventListener("touchend", stopUpdate);
+    window.removeEventListener("mouseup", stopUpdate);
+  }
+  function startUpdate() {
+    insertListeners();
+    let i = setInterval(() => {
+      fn(eventCopy, holdX, holdY);
+      if (!pressed) {
+        ejectListeners();
+        clearInterval(i);
+      }
+    }, t);
+  }
+  function stopUpdate() {
+    pressed = false;
+  }
+  function mousedown(e) {
+    // console.log('mousedown')
+    pressed = true;
+    updateMouse(e);
+    startUpdate();
+  }
+  function touchstart(e) {
+    pressed = true;
+    updateTouch(e);
+    startUpdate();
+  }
+  el.addEventListener("touchstart", touchstart);
+  el.addEventListener("mousedown", mousedown);
+}
+
+class Amuse {
+  async constructor(src, id, meta, load) {
+    if (src && id) {
+      this.addMuse(src, id, meta, load);
+    }
+  }
+  /************************************
+   * PUBLIC METHODS
+   */
+
+  async addMuse(src, id = false, meta = {}, load = true) {
+    if (!id) {
+      throw new Error("No id given to Muse constructor");
+    } else if (this.getMuseById(id)) {
+      throw new Error(
+        "Used Id given to Muse constructor. Unique muse id is required."
+      );
+    } else if (typeof src == "string") {
+      this._muses.push({
+        src,
+        id,
+        meta,
+      });
+      if (load) await this.load(id);
+      this._updateElements();
+    }
+  }
+
+  async removeMuseById(id) {
+    const museIndexToRemove = this._getMuseIndexById(id);
+    if (isNaN(museIndexToRemove))
+      throw new Error("Muse id doesnt exist, cannot remove the muse");
+    function remove(amuse) {
+      amuse._muses.splice(museIndexToRemove, 1);
+    }
+    if (museIndexToRemove === this._currentMuseIndex) {
+      await this.stop();
+
+      if (await this.next()) {
+        remove(this);
+        this._currentMuseIndex--;
+      } else {
+        await this.previous();
+        remove(this);
+      }
+    } else if (museIndexToRemove > this._currentMuseIndex) {
+      remove(this);
+    } else if (museIndexToRemove < this._currentMuseIndex) {
+      remove(this);
+      this._currentMuseIndex--;
+    }
+  }
+
+  getMuseById(id) {
+    let muse = this._muses.find((muse) => muse.id === id);
+    if (muse) return muse;
+    else return false;
+  }
+
+  async load(id) {
+    if (id == undefined) {
+      if (this.currentMuse) id = this.currentMuse.id;
+      else id = this._muses[0].id;
+    }
+    if (!this.getMuseById(id)) {
+      throw new Error("No muse exists with the id passed to load()");
+    } else {
+      let muse = this._muses.find((muse) => muse.id === id);
+      // if not loaded
+      if (!muse.audio) {
+        //load
+        muse.audio = new Audio(muse.src);
+        // setup event listeners for audio element
+        for (e in this._audioEventListeners) {
+          muse.audio.addEventListener(e, this._audioEventListeners[e]);
+        }
+
+        let int = setInterval(() => {
+          if (muse.audio.readyState >= 1) {
+            this._on("load");
+            this._updateElements();
+            clearInterval(int);
+            return true;
+          }
+        }, 5);
+      } else return true;
+    }
+  }
+
+  async play() {
+    // the user's event listener is called from this._audioEventListeners
+    await this.load();
+
+    this.currentMuse.audio.play();
+    this._updateElements();
+    return true;
+  }
+
+  async pause() {
+    // the user's event listener is called from this._audioEventListeners
+    await this.load();
+    this.currentMuse.audio.pause();
+    this._updateElements();
+    return true;
+  }
+
+  async stop() {
+    await this.pause();
+    this.currentSeconds = 0;
+    this._on("stop");
+    this._updateElements();
+    return true;
+  }
+
+  async next() {
+    //if this is the last one return false
+    if (this._currentMuseIndex + 1 >= this._muses.length) {
+      return false;
+    } else {
+      if (this.rememberTime) await this.pause();
+      else await this.stop();
+
+      this._currentMuseIndex++;
+      await this.play();
+
+      this._on("next");
+      this._on("skip");
+
+      return true;
+    }
+  }
+
+  async previous() {
+    if (this._currentMuseIndex === 0) {
+      return false;
+    } else {
+      if (this.rememberTime) await this.pause();
+      else await this.stop();
+
+      this._currentMuseIndex--;
+      await this.play();
+
+      this._on("previous");
+      this._on("skip");
+
+      return true;
+    }
+  }
+
+  async skipToId(id) {
+    if (isNaN(this._getMuseIndexById(id))) return false;
+    else return await this.skipToIndex(this._getMuseIndexById(id));
+  }
+
+  async skipToIndex(i) {
+    if (i >= 0 && i < this._muses.length) {
+      if (this.rememberTime) await this.pause();
+      else await this.stop();
+      this._currentMuseIndex = i;
+      await this.play();
+      this._on("skip");
+      return true;
+    } else return false;
+  }
+
+  async empty() {
+    await this.stop();
+    this._currentMuseIndex = 0;
+    this._muses = [];
+    this._updateElements();
+  }
+
+  async reset() {
+    await this.empty();
+    this.autoPlay = false;
+    this.rememberTime = true;
+    this._eventListeners.forEach((val) => (val = []));
+    this._elements.forEach((val) => (val = []));
+  }
+
+  addEventListener(type, callback) {
+    if (!this._eventListeners[type]) {
+      throw new Error(
+        "This type of event listener is not supported. Choose from " +
+          Object.keys(this._eventListeners).join(" | ")
+      );
+    } else if (typeof callback != "function") {
+      throw new Error("No callback function passed to Amuse.addEventistener()");
+    } else {
+      this._eventListeners[type].push(callback);
+    }
+  }
+
+  removeEventListener(type, callback) {
+    if (!this._eventListeners[type]) {
+      throw new Error(
+        "This type of event listener is not supported. Choose from " +
+          Object.keys(this._eventListeners).join(" | ")
+      );
+    } else if (typeof callback != "function") {
+      throw new Error("No callback function passed to Amuse.addEventistener()");
+    } else {
+      const i = this._eventListeners[type].indexOf(callback);
+      if (i < 0) throw new Error("No such eventListener exists.");
+      else this._eventListeners[type].splice(i, 1);
+    }
+  }
+
+  on(type, callback) {
+    this.addEventListener(type, callback);
+  }
+
+  notOn(type, callback) {
+    this.removeEventListener(type, callback);
+  }
+
+  once(type, callback) {
+    this.on(type, function (amuse) {
+      callback(amuse);
+      amuse.notOn(type, this);
+    });
+  }
+
+  addSeekBar(outer, inner, t = 0) {
+    if (!inner || !outer)
+      throw new Error("No elements to the .addSeekBar() method");
+    else {
+      this._elements.bar.push({ inner, outer });
+      onmousehold(
+        outer,
+        (e, x, y) => {
+          //seek the bar when you hold it
+          let barX = outer.getBoundingClientRect().left;
+          let barWidth = outer.clientWidth;
+          let percentage = (x - barX) / barWidth; // calculate the percentage
+
+          if (percentage >= 0 && percentage <= 1) {
+            // check if the mouse is insed the bar
+            this.currentPercentage = percentage;
+            this._updateElements();
+          }
+
+          this._on("seek");
+        },
+        t
+      );
+    }
+  }
+
+  addElement(type, el) {
+    if (!this._elements[type]) {
+      throw new Error(
+        "This type of element is not supported. Choose from " +
+          Object.keys(this._elements).join(" | ")
+      );
+    } else {
+      this._elements[type].push(el);
+    }
+    this._updateElements();
+  }
+  /***********************************
+   * PUBLIC SETTERS
+   */
+
+  set currentSeconds(s) {
+    if (s > this.duration || s < 0)
+      throw new Error("Can't seek to time out of the muse's range.");
+    else {
+      this.load().then(() => {
+        this.currentMuse.audio.currentTime = s;
+      });
+    }
+  }
+
+  set currentPercentage(p) {
+    this.currentSeconds = p * this.duration;
+  }
+
+  //set volume( v ) {}
+
+  /****************************************
+   * PUBLIC GETTERS
+   */
+
+  get loaded() {
+    return this.currentMuse ? (this.currentMuse.audio ? true : false) : false;
+  }
+
+  get playing() {
+    if (this.loaded && !this.currentMuse.audio.paused) {
+      return true;
+    } else return false;
+  }
+
+  get paused() {
+    return !this.playing;
+  }
+
+  get duration() {
+    if (this.loaded) {
+      let dur = this.currentMuse.audio.duration;
+      return !isNaN(dur) ? dur : 0;
+    } else return 0;
+  }
+
+  get currentSeconds() {
+    return this.loaded ? this.currentMuse.audio.currentTime : 0;
+  }
+
+  get currentPercentage() {
+    return this.currentSeconds / this.duration;
+  }
+
+  get currentMuse() {
+    if (this._muses.length === 0) return false;
+    else {
+      return this._muses[this._currentMuseIndex];
+    }
+  }
+
+  /**********************************
+   * PUBLIC PROPERTIES
+   */
+
+  autoPlay = false;
+
+  rememberTime = true;
+
+  /************************************
+   * PRIVATE
+   */
+
+  _muses = [];
+
+  _currentMuseIndex = 0;
+
+  _getMuseIndexById(id) {
+    return this._muses.findIndex((muse) => muse.id === id);
+  }
+
+  _eventListeners = {
+    play: [],
+    pause: [],
+    stop: [],
+    seek: [],
+    next: [],
+    previous: [],
+    skip: [],
+    playing: [],
+    load: [],
+    error: [],
+  };
+
+  _on(event, ...args) {
+    this._eventListeners[event].forEach((fn) => fn(this.currentMuse, ...args));
+  }
+
+  _elements = {
+    currentTime: [],
+    durationTime: [],
+    completeTime: [],
+    bar: [],
+    // to implement:
+    title: [],
+    nextBtn: [],
+    previousBtn: [],
+    playBtn: [],
+    album: [],
+    title: [],
+  };
+
+  get _museIds() {
+    return this._muses.map((val) => val.id);
+  }
+
+  get _audioEventListeners() {
+    return {
+      play: (e) => {
+        this._on("play");
+        this._updateElements();
+      },
+      pause: (e) => {
+        this._on("pause");
+        this._updateElements();
+      },
+      timeupdate: (e) => {
+        this._on("playing");
+        this._updateElements();
+      },
+      error: (e) => {
+        this._on("error", this);
+      },
+      ended: (e) => {
+        if (this.autoPlay) {
+          if (!this.next()) {
+            this.skipToIndex(0);
+          }
+        } else {
+          this.pause();
+        }
+      },
+    };
+  }
+
+  _updateElements() {
+    //update seekbar:
+    this._elements.bar.forEach((bar) => {
+      bar.inner.style.width = this.currentPercentage * 100 + "%";
+    });
+
+    let currentTime = {
+      min: Math.floor(this.currentTimerentSeconds / 60),
+      sec: Math.floor(this.currentTimerentSeconds % 60),
+    };
+    currentTime.sec < 10
+      ? (currentTime.sec = "0" + currentTime.sec)
+      : undefined;
+
+    let dur = {
+      min: this.loaded && this.duration ? Math.floor(this.duration / 60) : "--",
+      sec: this.loaded && this.duration ? Math.floor(this.duration % 60) : "--",
+    };
+    dur.sec < 10 ? (dur.sec = "0" + dur.sec) : undefined;
+
+    this._elements.currentTimerentTime.forEach((el) => {
+      el.innerText = `${currentTime.min}:${currentTime.sec}`;
+    });
+
+    this._elements.durationTime.forEach((el) => {
+      el.innerText = `${dur.min}:${dur.sec}`;
+    });
+
+    this._elements.completeTime.forEach((el) => {
+      el.innerText = `${currentTime.min}:${currentTime.sec} / ${dur.min}:${dur.sec}`;
+    });
+  }
+}
+
+export default Amuse;
